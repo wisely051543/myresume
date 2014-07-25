9dd2009ef1d4db2e48b2ce5bb7cffdba3e96a269
wiselyShao <wisely@thetigerparty.com>
2014-07-25 21:44:04 +0800

    initial commit gulp-angular generator (replace gulp styles task with gulp-compass)

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = '/'
+css_dir = 'app/assets/scss'
+sass_dir = 'app/assets/scss'
+images_dir = 'app/images'
+javascripts_dir = 'app/scripts'
+
+
+relative_assets = true
+line_comments = true
